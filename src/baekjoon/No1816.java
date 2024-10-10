53e17ae33ec57fe3c08497abc3e0adb488c717dc
ㅅㅎㅇ <ise0305@naver.com>
2024-10-11 05:09:32 +0900

    :sparkles: Brute-Force 암호키 문제

---
+++ src/baekjoon/No1816.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class No1816 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long[] arr = new long[sc.nextInt()];
+        String[] result = new String[arr.length];
+
+        boolean flag = false;
+
+        for (int i=0; i<arr.length; i++){
+            arr[i]= sc.nextLong();
+        }
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=2; j<=1000000; j++){
+                if(arr[i]%j==0){
+                    result[i]="NO";
+                    break;
+                }
+            }
+            if(result[i]==null || result[i].equals("")){
+                result[i]="YES";
+            }
+        }
+
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(result[i]);
+        }
+
+    }
+}
